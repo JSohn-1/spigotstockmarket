11ff5657a814544de4e8418d33ef7279fc4cad5f
ProEpicGamer1 <40511868+ProEpicGamer1@users.noreply.github.com>
2021-10-25 21:42:53 -0700

    Update price.py

--- price.py
+++ price.py
@@ -1,7 +1,7 @@
 import csv
 import finnhub
 
-finnhub_client = finnhub.Client(api_key="c5ro3eaad3ifnpn57lmg")
+finnhub_client = finnhub.Client(api_key="Your API Key")
 
 f = open("stocks.csv", "w")
 writer = csv.writer(f)
@@ -16,14 +16,3 @@ writer.writerow(header)
 addStock("APPL")
 
 f.close()
-
-#print(finnhub_client.company_eps_estimates('APPL', freq='quarterly'))
-
-import requests
-
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=15min&apikey=DECGTCKXGVWZB116'
-r = requests.get(url)
-data = r.json()
-
-print(data)
